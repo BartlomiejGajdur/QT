dedff4a4f7ef3fbe5cbfe34ec999c2111f67df4e
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-12-06 17:39:10 +0100

    Refactor

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[]){
 
     QApplication app(argc, argv);
-    QLabel *label = new QLabel("Hello World");
+    QLabel *label = new QLabel("Hello World :)");
     label->setWindowTitle("MY APP");
     label->resize(400,400);
     label->show();
